69364c9dd2d15548bfd4b2407a7ea140e2082407
Boris Aizenberg <boris_aizenberg@epam.com>
2025-01-13 14:53:27 +0500

    init

--- task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
+++ task1-strategy-pattern/src/main/java/com/mjc/stage2/ShopStock.java
@@ -1,5 +1,6 @@
 package com.mjc.stage2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShopStock {
@@ -9,5 +10,13 @@ public class ShopStock {
         this.productList = productList;
     }
 
-    // Write your code here!
+    public List executeFilteringStrategy(FilteringStrategy filteringStrategy){
+        List<Product> result = new ArrayList();
+        for (Product a : productList) {
+            if (filteringStrategy.filter(a)) {
+                result.add(a);
+            }
+        }
+        return result;
+    }
 }
--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -1,5 +1,20 @@
 package com.mjc.stage2.impl;
 
-public class RectangleObserver  {
+import com.mjc.stage2.Observer;
+import com.mjc.stage2.entity.Rectangle;
+import com.mjc.stage2.entity.RectangleValues;
+import com.mjc.stage2.event.RectangleEvent;
+import com.mjc.stage2.warehouse.RectangleWarehouse;
+
+public class RectangleObserver implements Observer {
+    @Override
+    public void handleEvent(RectangleEvent event) {
+        Rectangle rect = event.getSource();
+        RectangleWarehouse wh = RectangleWarehouse.getInstance();
+        RectangleValues old = wh.get(rect.getId());
+        RectangleValues values = new RectangleValues(rect.square(), rect.perimeter());
+        wh.remove(rect.getId(), old);
+        wh.put(rect.getId(), values);
+    }
     // Write your code here!
 }
